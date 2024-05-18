6cad8b9de1103f1893b6b7e7185a0ba554b7b4e6
Malyakin <5nvmbr5@gmail.com>
2024-05-18 15:04:23 +0300

    Practice 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Practice 5',
+      title: 'Practice 8',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -125,17 +126,31 @@ class NewScreen4 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Список URL-адресов изображений
+    final List<String> imageUrls = [
+      'https://cdn.tripster.ru/photos/41d79b35-f2c2-48eb-97d6-d4c69e8e539d.jpg',
+      'https://masterpiecer-images.s3.yandex.net/e8d93bf5884411eebe671e5d9776cfa6:upscaled',
+      'https://assets.change.org/photos/9/au/lq/NQAULqXWrTgtwxH-800x450-noPad.jpg?1529671287',
+      'https://cdnuploads.aa.com.tr/uploads/Contents/2022/12/10/thumbs_b_c_7e823d2d2bddb80ba76e8a4b041a1a9b.jpg?v=002153',
+      'https://avatars.dzeninfra.ru/get-zen_doc/138668/pub_5ac4796f256d5c91f783fe17_5ac4b215f03173368dd63fb9/scale_1200',
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Screen 4'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/screen5');
-          },
-          child: const Text('Go to Screen 5'),
-        ),
+      body: ListView.builder(
+        itemCount: imageUrls.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CachedNetworkImage(
+              imageUrl: imageUrls[index],
+              placeholder: (context, url) => const CircularProgressIndicator(),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
+          );
+        },
       ),
     );
   }
@@ -161,6 +176,3 @@ class NewScreen5 extends StatelessWidget {
     );
   }
 }
-
-
-
